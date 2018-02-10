3be3437f657935a115cdec413c889695a5b3e392
Felix Borchers <h1f1x@users.noreply.github.com>
2018-02-10 09:39:20 +0100

    Refactor wait_for_stacks (moved)

--- src/main/python/aws_account_janitor/cloudformation.py
+++ src/main/python/aws_account_janitor/cloudformation.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+import time
 
 from aws_account_janitor.logging import log
 
@@ -25,3 +26,34 @@ def cleanup(status, interactive=True):
         for stack_name in stacks:
             log('initiating deletion of "{}" ...'.format(stack_name))
             cf.delete_stack(StackName=stack_name)
+
+
+def wait_for_status_change(status, timeout):
+    cf = boto3.client('cloudformation')
+    all_stacks_changed = False
+    click.echo('Looking for {} stacks and wait (max. {}s) for them to change their status.'.format(
+        status, timeout))
+    wait_until = time.time() + timeout
+    pending = {n['StackName'] for n in cf.list_stacks(
+        StackStatusFilter=status.split())['StackSummaries']}
+    log('>> Found {} stacks.'.format(len(pending)))
+    for stack in pending:
+        log('- {}'.format(stack))
+    log('Waiting ...')
+
+    while True:
+        if len(pending) < 1:
+            print('<< Voila, all found stacks changed its status.')
+            all_stacks_changed = True
+            break
+        if time.time() >= wait_until:
+            print('<< Got bored of waiting. Timeout after: {} s.'.format(timeout))
+            break
+        time.sleep(2)
+        stacks = {n['StackName'] for n in cf.list_stacks(
+            StackStatusFilter=status.split())['StackSummaries']}
+        changed = pending - stacks
+        for stack in changed:
+            log('{} - changed status'.format(stack))
+        pending = stacks & pending
+    return all_stacks_changed
