3585469c397bccfb12a6cddc0c78eedc86a458c1
Felix Borchers <h1f1x@users.noreply.github.com>
2018-10-23 16:41:14 +0200

    Add dynamodb: cleanup tables

--- src/main/python/aws_account_janitor/dynamodb.py
+++ src/main/python/aws_account_janitor/dynamodb.py
@@ -12,3 +12,13 @@ def cleanup_backups(dry_run=False):
                 log_prefix, backup['BackupName'], backup['TableName']))
             if not dry_run:
                 client.delete_backup(BackupArn=backup['BackupArn'])
+
+
+def cleanup_tables(dry_run=False):
+    for tables in client.get_paginator('list_tables').paginate():
+        for table in tables['TableNames']:
+            log_prefix = '[DRY RUN] ' if dry_run else ''
+            log('{}deleting table: {} ...'.format(
+                log_prefix, table))
+            if not dry_run:
+                client.delete_table(TableName=table)
